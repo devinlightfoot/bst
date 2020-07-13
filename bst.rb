1eb76788eef28dea37ac01e5168852ca6fd1601b
devinlightfoot <euler2171828@gmail.com>
2020-07-13 00:01:50 -0700

    finished bfs, working on fixing build_tree&dfs

--- bst.rb
+++ bst.rb
@@ -1,159 +1,236 @@
-def Node
-    include Comparable
-    attr_accessor :data, :left, :right
-    def initialize(data = nil)
-        @data = data
-        @left = left
-        @right = right
-    end
+# frozen_string_literal: true
 
+class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+  def initialize(data = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
 end
-def Tree
-    attr_accessor :root
-    def initialize(arr)
-        @root = build_tree(arr)
-    end
-    def build_tree(arr)
-        arr.uniq.sort
-        root, tmp_root = nil
-        cond = true
-        arr.each do |val|
-            if root.nil?
-                root = Node.new(val)
-            else
-                if val >= root.data
-                    if root.right.present?
-                        tmp_root = root.right
-                        while cond
-                           if tmp_root.left.present? && tmp_root.right.present?
-                                if val >= tmp_root.data
-                                    tmp_root = tmp_root.right
-                                else
-                                    tmp_root = tmp_root.left
-                                end
+class Tree
+  attr_accessor :root
+  def initialize(arr)
+    @root = build_tree(arr)
+  end
+
+  # broken
+  def build_tree(arr)
+    arr.uniq.sort
+    root, tmp_root = nil
+    cond = true
+    arr.each do |val|
+      if root.nil?
+        root = Node.new(val)
+      else
+        if val >= root.data
+          if !root.right.nil?
+            tmp_root = root.right
+            while cond
+              if !tmp_root.left.nil? && !tmp_root.right.nil?
+                tmp_root = if val >= tmp_root.data
+                             tmp_root.right
                            else
-                                if val >= tmp_root.data
-                                    tmp_root.right = Node.new(val)
-                                else
-                                    tmp_root.left = Node.new(val)
-                                end
-                                cond = false
-                           end
-                        end
-                    else
-                        root.right = Node.new(val)
-                    end
-                elsif val < root.data
-                    if root.left.present?
-                        tmp_root = root.left
-                        while cond
-                            if tmp_root.left.present? && tmp_root.right.present?
-                                if val >= tmp_root.data
-                                    tmp_root = tmp_root.right
-                                else
-                                    tmp_root = tmp_root.left
-                                end
-                            else
-                                if val >= tmp_root.data
-                                    tmp_root.right = Node.new(val)
-                                else
-                                    tmp_root = Node.new(val)
-                                end
-                                cond = false
-                            end
-                        end
-                    else
-                        root.left = Node.new(val)
-                    end
-                end
-            end 
-            cond = true
-        end
-        return root
-    end
-    def insert(val)
-        cond = true
-        tmp = @root
-        while cond
-            if @root.nil?
-                build_tree(val)
-            elsif tmp.left.present? && tmp.right.present?
-                if val >= tmp.data
-                    tmp = tmp.right
+                             tmp_root.left
+                             end
+              else
+                if val >= tmp_root.data
+                  tmp_root.right = Node.new(val)
                 else
-                    tmp = tmp.left
-                end
-            else
+                  tmp_root.left = Node.new(val)
+                  end
                 cond = false
+              end
             end
-        end
-        if val >= tmp.data
-            tmp.right = Node.new(val)
-        else
-            tmp.left = Node.new(val)
-    end
-    def delete(val)
-        cond = true
-        tmp = @root
-       while cond
-            if @root.data == val && @root.left.nil? && @root.right.nil?
-                cond = false
-                return nil
-            elsif tmp.data != val && tmp.left.nil? && tmp.right.nil?
-                return nil
-            elsif @root.data == val && @root.left.present? && @oot.right.present?
-                @root = build_tree(deconstruct(@root,val))
-            elsif tmp.left.data == val
-                tmp = build_tree(deconstruct(tmp,val))
-                cond = false
-            elsif tmp.right.data == val
-            tmp = build_tree(deconstruct(tmp,val))
-            cond = false
-        else
-            if val > tmp.data
-                tmp = tmp.right
-            elsif val < tmp.data
-                tmp = tmp.left
-            end
-       end
-    end
-    #helper method for delete(val)
-    def deconstruct(node,except = nil)
-        arr = [node]
-        cond = true
-        while cond
-            next if node.data == except
-            if node.left.present? && node.right.present?
-                arr.push(deconstruct(node.left,except))
-                arr.push(deconstruct(node.right,except))
-            else
-                cond = false
-            end
-        end
-        arr.flatten
-        return arr
-    end
-    def find(val)
-        cond = true
-        tmp = @root
-        while cond
-            return nil if @root.nil?
-            if tmp.data = val
-                cond = true
-                return tmp
-            elsif tmp.left.data = val
-                return tmp.elft
-            elsif tmp.right.data = val
-                return tmp.right
-            elsif tmp.left.nil? && tmp.right.nil? && tmp.data != val
-                return nil
-            else
-                if val > tmp.data
-                    tmp = tmp.right
+          else
+            root.right = Node.new(val)
+          end
+        elsif val < root.data
+          if !root.left.nil?
+            tmp_root = root.left
+            while cond
+              if !tmp_root.left.nil? && !tmp_root.right.nil?
+                tmp_root = if val >= tmp_root.data
+                             tmp_root.right
+                           else
+                             tmp_root.left
+                             end
+              else
+                if val >= tmp_root.data
+                  tmp_root.right = Node.new(val)
                 else
-                    tmp = tmp.left
-                end
+                  tmp_root = Node.new(val)
+                  end
+                cond = false
+              end
             end
-        end
+          else
+            root.left = Node.new(val)
+          end
+          end
+      end
+      cond = true
     end
+    root
+  end
+
+  # works?
+  def insert(val)
+    cond = true
+    tmp = @root
+    while cond
+      if @root.nil?
+        build_tree(val)
+      elsif !tmp.left.nil? && !tmp.right.nil?
+        tmp = if val >= tmp.data
+                tmp.right
+              else
+                tmp.left
+                end
+      else
+        cond = false
+      end
+    end
+    if val >= tmp.data
+      tmp.right = Node.new(val)
+    else
+      tmp.left = Node.new(val)
+    end
+  end
+
+  # works?
+  def delete(val)
+    cond = true
+    tmp = @root
+    while cond
+      if @root.data == val && @root.left.nil? && @root.right.nil?
+        cond = false
+        return nil
+      elsif tmp.data != val && tmp.left.nil? && tmp.right.nil?
+        return nil
+      elsif @root.data == val && @root.left.nil? && @oot.right.nil?
+        @root = build_tree(preorder(@root, val))
+      elsif tmp.left.data == val
+        tmp = build_tree(preorder(tmp, val))
+        cond = false
+      elsif tmp.right.data == val
+        tmp = build_tree(preorder(tmp, val))
+        cond = false
+      else
+        if val > tmp.data
+          tmp = tmp.right
+        elsif val < tmp.data
+          tmp = tmp.left
+          end
+      end
+    end
+  end
+
+  # works?
+  def find(val)
+    cond = true
+    tmp = @root
+    while cond
+      return nil if @root.nil?
+
+      if tmp.data = val
+        cond = true
+        return tmp.data
+      elsif tmp.left.data = val
+        return tmp.left.data
+      elsif tmp.right.data = val
+        return tmp.right.data
+      elsif tmp.left.nil? && tmp.right.nil? && tmp.data != val
+        return nil
+      else
+        tmp = if val > tmp.data
+                tmp.right
+              else
+                tmp.left
+              end
+      end
+    end
+  end
+
+  # broken
+  def level_order
+    arr = []
+    q = []
+    q.push(@root)
+    cond = true
+    while cond
+      break if q.empty?
+
+      tmp = q[0]
+      arr.push(tmp.data)
+      if !tmp.left.nil?
+        q.push(tmp.left)
+      elsif !tmp.right.nil?
+        q.push(tmp.right)
+      end
+      q.pop
+    end
+    arr
+  end
+
+  def pre_order(node = @root, &block)
+    return if node.nil?
+
+    yield node
+    in_order(node.left, &block)
+    in_order(node.right, &block)
+  end
+
+  def in_order(node = @root, &block)
+    return if node.nil?
+
+    in_order(node.left, &block)
+    yield node
+    in_order(node.right, &block)
+   end
+
+  def post_order(node = @root, &block)
+    return if node.nil?
+
+    in_order(node.left, &block)
+    in_order(node.right, &block)
+    yield node
+  end
+
+  def height(node)
+    return 0 if node.nil?
+
+    leftHeight = check_height(node.left)
+    return -1 if leftHeight == -1
+
+    rightHeight = check_height(node.right)
+    return -1 if rightHeight == -1
+
+    diff = leftHeight - rightHeight
+    if diff.abs > 1
+      -1
+    else
+      [leftHeight, rightHeight].max + 1
+    end
+  end
+
+  def is_balanced?(node = @root)
+    check_height(node) != -1
+  end
+end
+tree = Tree.new(Array.new(15) { rand(1..100) })
+puts 'pre_order'
+tree.pre_order do |node|
+  puts node.data
+end
+
+puts 'in_order'
+tree.in_order do |node|
+  puts node.data
+end
+
+puts 'post_order'
+tree.post_order do |node|
+  puts node.data
 end
